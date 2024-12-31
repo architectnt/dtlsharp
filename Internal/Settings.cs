b41ad020b211c3b1433b6d91f882b9b6f3cf8f1b
architectnt <eno@i2iproductions.com>
2024-12-30 21:34:16 -0800

    p

---
+++ Internal/Settings.cs
@@ -0,0 +1,11 @@
+/*
+    This is a part of DigitalOut and is licenced under MIT.
+*/
+
+namespace dtl.Internal {
+    public class Settings{
+        public string token;
+        public string catboxuser;
+        public string[] statuses;
+    }
+}
